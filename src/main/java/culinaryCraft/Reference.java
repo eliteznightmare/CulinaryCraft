7db04eaf6dfdbdb04392d69827f8a0b1ce51becf
Eliteznightmare <matt.hman@yahoo.com>
2014-09-08 17:14:00 -0400

    updated references

--- src/main/java/culinaryCraft/Reference.java
+++ src/main/java/culinaryCraft/Reference.java
@@ -4,4 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "CulinaryCraft";
 	public static final String MOD_NAME = "CulinaryCraft";
 	public static final String MOD_VERSION = "1.7.10-A-1.0";
+	public static final String GUI_FACTORY_CLASS = "culinaryCraft.client.gui.ForgeGuiFactory";
 }
